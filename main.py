b89ddf3080236fe8677f8256ece9732a8e1d1d5e
fnnetwork <fnnetwork272@gmail.com>
2025-01-21 14:37:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -182,4 +182,5 @@ if __name__ == "__main__":
     dispatcher.add_handler(MessageHandler(TEXT, handle_message))
     dispatcher.add_handler(CallbackQueryHandler(callback_handler))
 
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
